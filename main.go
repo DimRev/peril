83d4fcea81b315b5affe4c9e765e2a2291173b83
DimRev <dimrev444@gmail.com>
2024-05-03 08:40:19 +0300

    Add starter files

--- main.go
+++ main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"syscall"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	const connStr = "amqp://guest:guest@localhost:5672/"
-	conn, err := amqp.Dial(connStr)
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(rabbitConnString)
 	if err != nil {
-		log.Printf("Failed to connect to RabbitMQ, %v", err)
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
 	}
 	defer conn.Close()
+	fmt.Println("Peril game server connected to RabbitMQ!")
 
-	log.Println("Successfully connected to RabbitMQ")
-
+	// Wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	fmt.Println("Waiting for interrupt signal (Ctrl+C)...")
 	<-signalChan
+	fmt.Println("\nReceived interrupt signal. Exiting...")
 }
